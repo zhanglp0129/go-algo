e5103ac02f74e87f339c4c9300fe9cb21e41e7a1
zhanglp0129 <zhanglp0129@gmail.com>
2024-05-16 22:52:02 +0800

    优化了队列源码，并提供了测试用例

--- queue.go
+++ queue.go
@@ -7,28 +7,42 @@ type Queue[T any] struct {
 
 // NewQueue 创建队列
 func NewQueue[T any]() *Queue[T] {
-	var q Queue[T]
-	q.data = make([]T, 15) // 队列初始容量为15
-	q.front, q.rear = 0, 0
-	return &q
+	return &Queue[T]{0, 0, make([]T, 15)}
 }
 
 // Push 添加一个元素到队尾
 func (q *Queue[T]) Push(val T) {
 	if q.Size()+1 == cap(q.data) {
 		t := make([]T, 2*cap(q.data))
-		for i := 0; i < cap(q.data)-1; i++ {
+		size := cap(q.data) - 1
+		for i := 0; i < size; i++ {
 			t[i] = q.Pop()
 		}
-
-		q.front, q.rear = 0, cap(q.data)-1
-		q.data = t
+		q.front, q.rear, q.data = 0, size, t
 	}
 	q.data[q.rear] = val
 	q.rear++
 	if q.rear == cap(q.data) {
 		q.rear = 0
 	}
+}
+
+// Pop 弹出队首元素，并返回
+func (q *Queue[T]) Pop() T {
+	t := q.Front()
+	q.front++
+	if q.front == cap(q.data) {
+		q.front = 0
+	}
+	return t
+}
+
+// Front 返回队首元素
+func (q *Queue[T]) Front() T {
+	if q.Empty() {
+		panic("不能从空队列中取元素")
+	}
+	return q.data[q.front]
 }
 
 // Size 求队列大小
@@ -42,23 +56,5 @@ func (q *Queue[T]) Size() int {
 
 // Empty 判断队列是否为空
 func (q *Queue[T]) Empty() bool {
-	return q.Size() == 0
-}
-
-// Pop 弹出队首元素，并返回
-func (q *Queue[T]) Pop() T {
-	if q.Empty() {
-		panic("空队列不能弹出元素！")
-	}
-	t := q.Front()
-	q.front++
-	if q.front == cap(q.data) {
-		q.front = 0
-	}
-	return t
-}
-
-// Front 返回队首元素
-func (q *Queue[T]) Front() T {
-	return q.data[q.front]
+	return q.front == q.rear
 }
