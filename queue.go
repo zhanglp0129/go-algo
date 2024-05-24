4115afda19997a665c0fe36de800f6ebfba8b6d0
zhanglp0129 <zhanglp0129@gmail.com>
2024-05-24 20:49:21 +0800

    增加了一些注释

--- queue.go
+++ queue.go
@@ -12,26 +12,26 @@ func NewQueue[T any]() *Queue[T] {
 
 // Push 添加一个元素到队尾
 func (q *Queue[T]) Push(val T) {
-	if q.Size()+1 == cap(q.data) {
+	if q.Size()+1 == cap(q.data) { // 队列满了，需要扩容
 		t := make([]T, 2*cap(q.data))
 		size := cap(q.data) - 1
-		for i := 0; i < size; i++ {
+		for i := 0; i < size; i++ { // 将循环队列拉直
 			t[i] = q.Pop()
 		}
 		q.front, q.rear, q.data = 0, size, t
 	}
 	q.data[q.rear] = val
 	q.rear++
-	if q.rear == cap(q.data) {
+	if q.rear == cap(q.data) { // 队尾+1后，下标不能越界
 		q.rear = 0
 	}
 }
 
 // Pop 弹出队首元素，并返回
 func (q *Queue[T]) Pop() T {
-	t := q.Front()
+	t := q.Front() // 取出队首元素
 	q.front++
-	if q.front == cap(q.data) {
+	if q.front == cap(q.data) { // 队首+1后，下标不能越界
 		q.front = 0
 	}
 	return t
@@ -48,7 +48,7 @@ func (q *Queue[T]) Front() T {
 // Size 求队列大小
 func (q *Queue[T]) Size() int {
 	size := q.rear - q.front
-	if size < 0 {
+	if size < 0 { // 大小不能为负数
 		return size + cap(q.data)
 	}
 	return size
